9b1ef75bc5eb0e3b0ac3a6281ecba117277d0636
lovac42 <lovac42@protonmail.com>
2020-04-02 14:35:21 -0400

    Updated copyright to 2020

--- src/cronholio/const.py
+++ src/cronholio/const.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright: (C) 2018 Lovac42
+# Copyright: (C) 2018-2020 Lovac42
 # Support: https://github.com/lovac42/Cronholio
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.2
 
-
-# == User Config =========================================
-
-# Moved. Edit the config.json file
-
-# == End Config ==========================================
-##########################################################
 
 from anki import version
 ANKI21 = version.startswith("2.1.")
